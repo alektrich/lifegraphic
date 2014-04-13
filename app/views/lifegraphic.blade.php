f30586e47efa808d41b3b0185548bd0708fe21ce
alektrich <alektrich@gmail.com>
2014-04-13 15:58:16 +0200

    Initial commit

---
+++ app/views/lifegraphic.blade.php
@@ -0,0 +1,287 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Title of the document</title>
+<link href="styles.css" rel="stylesheet" type="text/css">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<script src="http://d3js.org/d3.v3.min.js" charset="utf-8"></script>
+<style type="text/css">
+.container {
+  clear: both;
+  height: auto;
+  width: 1024px;
+  border-top-width: 1px;
+  border-right-width: 1px;
+  border-bottom-width: 1px;
+  border-left-width: 1px;
+  border-top-style: solid;
+  border-right-style: solid;
+  border-bottom-style: solid;
+  border-left-style: solid;
+  margin: 0 auto;
+  ;
+  /* [disabled]background-image: url(lifeographic.jpg); */
+}
+.Header {
+  float: left;
+  height: auto;
+  width: 1024px;
+}
+.Leftpanel {
+  background-color: #444;
+  float: left;
+  height: auto;
+  width: 150px;
+  margin-top: 5px;
+}
+.progressbar {
+  float: left;
+  height: auto;
+  width: 782px;
+  margin-left: 15px;
+  background-color: #3CF;
+}
+.Topsection {
+  float: left;
+  height: auto;
+  width: 782px;
+  margin-left: 15px;
+}
+.graphdiv {
+  float: left;
+  height: auto;
+  width: 782px;
+  margin-left: 15px;
+  background-color: #3F6;
+}
+.catbar {
+  float: left;
+  height: auto;
+  width: 782px;
+  margin-left: 15px;
+  background-color: #3FF;
+}
+.healthbar_div {
+  background-color: #DDD;
+  height: 40px;
+  width: 250px;
+  margin-left: 27px;
+}
+.datestamp_div {
+  float: right;
+  height: 20px;
+  width: 125px;
+  margin-right: 15px;
+}
+.Time_div {
+  float: right;
+  height: 15px;
+  width: 100px;
+  margin-right: 27px;
+}
+.subtitle_div {
+  background-color: #DDD;
+  float: left;
+  height: 27px;
+  width: 275px;
+  margin-left: 27px;
+  margin-top: 5px;
+}
+.rank_div {
+  background-color: #DDD;
+  float: left;
+  height: 55px;
+  width: 400px;
+  margin-left: 27px;
+}
+.historygraph {
+  background-color: #DDD;
+  float: left;
+  height: 25px;
+  width: 250px;
+  margin-left: 170px;
+}
+.historygraphbox_div {
+  background-color: #3CF;
+  float: right;
+  height: 180px;
+  width: 280px;
+  margin-right: 25px;
+}
+.Reason_div {
+  float: left;
+  height: 100px;
+  width: 435px;
+  margin-left: 27px;
+}
+.rankingboxes_div {
+  float: left;
+  height: 123px;
+  width: 123px;
+  margin-left: 30px;
+  background-color: #0C9;
+}
+.rankingbox2_div {
+  background-color: #3FF;
+  float: left;
+  height: 123px;
+  width: 123px;
+  margin-left: 5px;
+}
+.LifeDial_div {
+  float: left;
+  height: 123px;
+  width: 198px;
+  margin-left: 8px;
+}
+.rankingbox3_div {
+  background-color: #CC9;
+  float: left;
+  height: 123px;
+  width: 123px;
+  margin-left: 5px;
+}
+.rankingbox4_div {
+  background-color: #6F9;
+  float: left;
+  height: 123px;
+  width: 123px;
+  margin-left: 5px;
+}
+.totalgraph_div {
+  float: left;
+  height: 170px;
+  width: 725px;
+  margin-left: 27px;
+  background-color: #0C6;
+}
+.leftpanel_rows {
+  float: left;
+  height: 25px;
+  width: 145px;
+  margin-left: 5px;
+  margin-top: 10px;
+}
+
+</style>
+</head>
+
+<body>
+<div class="container">
+  <div class="Header">
+    <p>Header Title</p>
+  </div>
+  <div class="Leftpanel">
+<div class="leftpanel_rows">Status {{$test}}</div>
+<div class="leftpanel_rows">My Profile</div>
+<div class="leftpanel_rows">Calendar</div>
+<div class="leftpanel_rows">Question</div>
+<div class="leftpanel_rows">Forum</div>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>Goes Here</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+  </div>
+  <p>&nbsp;</p>
+  <div class="progressbar">
+    <p>Content for  class "progressbar" Goes Here</p>
+    <p>&nbsp;</p>
+  </div>
+  <div class="Topsection">
+   <div class="Time_div">Time: 13:00</div>
+    <div class="datestamp_div">Date: 01/01/2014</div>
+    <div class="healthbar_div">Health Bar</div>
+   
+    
+    <div class="subtitle_div">How Are you Feeling Today?</div>
+    
+    <div class="historygraph">Your Health History</div>
+  <div class="historygraphbox_div">Andreja</div>
+    <div class="rank_div">Ranking goes here</div>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <div class="Reason_div">Content for  class "Reason_div" Goes Here</div>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <div class="rankingboxes_div">Content for  class "rankingboxes_div" Goes Here</div>
+    <div class="rankingbox2_div">Content for  class "rankingbox2_div" Goes Here</div>
+    <div class="LifeDial_div">Content for  class "LifeDial_div" Goes Here</div>
+    <div class="rankingbox3_div">Content for  class "rankingbox3_div" Goes Here</div>
+    <div class="rankingbox4_div">Content for  class "rankingbox4_div" Goes Here</div>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <div class="totalgraph_div">Content for  class "totalgraph_div" Goes Here</div>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+  </div>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <div class="catbar">
+    <p>Content for  class "catbar" Goes Here</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+  </div>
+  <div class="catbar">
+    <p>Content for  class "catbar" Goes Here    </p>
+    <p>&nbsp;</p>
+  </div>
+  <div class="catbar">
+    <p>Content for  class "catbar" Goes Here    </p>
+    <p>&nbsp;</p>
+  </div>
+  <div class="catbar">
+    <p>Content for  class "catbar" Goes Here </p>
+    <p>&nbsp;</p>
+  </div>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+</div>
+</body>
+
+</html>
