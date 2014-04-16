ea53dc6933e61695c9c08a12b7ddeab5125221d4
Aleksandar Trickovic <alektrich@gmail.com>
2014-04-16 10:41:02 +0200

    Upgrading User.php model to new Laravel version

--- app/models/User.php
+++ app/models/User.php
@@ -62,4 +62,25 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 		return $this->email;
 	}
 
+	/**
+	 * The following three methods are necessary for updating Laravel to 4.1.26.
+	 *
+	 * @return string
+	 */
+
+	public function getRememberToken()
+	{
+	    return $this->remember_token;
+	}
+
+	public function setRememberToken($value)
+	{
+	    $this->remember_token = $value;
+	}
+
+	public function getRememberTokenName()
+	{
+	    return 'remember_token';
+	}
+
 }
