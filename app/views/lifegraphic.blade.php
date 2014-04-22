fd6dc3674a6080bcc8be447cd21948f190b640a8
alektrich <alektrich@gmail.com>
2014-04-22 12:33:44 +0200

    Creating history graph

--- app/views/lifegraphic.blade.php
+++ app/views/lifegraphic.blade.php
@@ -58,7 +58,9 @@
       <div class="subtitle_divDash">How Are you Feeling Today?</div>
       
       <div class="historygraphDash">Your Health History</div>
-    <div class="historygraphbox_divDash">history graph</div>
+    <div class="historygraphbox_divDash">
+    <canvas id="historyGraphCanvas" width="280" height="162"></canvas>
+    </div>
       <div class="rank_divDash">Ranking goes here</div>
       <p>&nbsp;</p>
       <p>&nbsp;</p>
@@ -132,7 +134,113 @@
 @section('script')
   <script>
 
-    var options = {
+
+    var dataGraph = {
+                  labels : ["1","2","3","4","5","6","7"],
+                  datasets : [
+                    {
+                      fillColor : "rgba(159, 237, 139, 0.39)",
+                      strokeColor : "rgba(220,220,220,1)",
+                      pointColor : "rgba(220,220,220,1)",
+                      pointStrokeColor : "#fff",
+                      data : [65,59,90,81,56,55,40]
+                    }/*,
+                    {
+                      fillColor : "rgba(151,187,205,0.5)",
+                      strokeColor : "rgba(151,187,205,1)",
+                      pointColor : "rgba(151,187,205,1)",
+                      pointStrokeColor : "#fff",
+                      data : [28,48,40,19,96,27,100]
+                    }*/
+                  ]
+                }
+                
+    var optionsGraph = {
+        //Boolean - If we show the scale above the chart data     
+        scaleOverlay : false,
+        
+        //Boolean - If we want to override with a hard coded scale
+        scaleOverride : false,
+        
+        //** Required if scaleOverride is true **
+        //Number - The number of steps in a hard coded scale
+        scaleSteps : null,
+        //Number - The value jump in the hard coded scale
+        scaleStepWidth : null,
+        //Number - The scale starting value
+        scaleStartValue : null,
+
+        //String - Colour of the scale line 
+        scaleLineColor : "rgba(0,0,0,.1)",
+        
+        //Number - Pixel width of the scale line  
+        scaleLineWidth : 1,
+
+        //Boolean - Whether to show labels on the scale 
+        scaleShowLabels : true,
+        
+        //Interpolated JS string - can access value
+        scaleLabel : "<%=value%>",
+        
+        //String - Scale label font declaration for the scale label
+        scaleFontFamily : "'Arial'",
+        
+        //Number - Scale label font size in pixels  
+        scaleFontSize : 12,
+        
+        //String - Scale label font weight style  
+        scaleFontStyle : "normal",
+        
+        //String - Scale label font colour  
+        scaleFontColor : "#999",  
+        
+        ///Boolean - Whether grid lines are shown across the chart
+        scaleShowGridLines : true,
+        
+        //String - Colour of the grid lines
+        scaleGridLineColor : "rgba(0,0,0,.05)",
+        
+        //Number - Width of the grid lines
+        scaleGridLineWidth : 1, 
+        
+        //Boolean - Whether the line is curved between points
+        bezierCurve : true,
+        
+        //Boolean - Whether to show a dot for each point
+        pointDot : true,
+        
+        //Number - Radius of each point dot in pixels
+        pointDotRadius : 3,
+        
+        //Number - Pixel width of point dot stroke
+        pointDotStrokeWidth : 1,
+        
+        //Boolean - Whether to show a stroke for datasets
+        datasetStroke : true,
+        
+        //Number - Pixel width of dataset stroke
+        datasetStrokeWidth : 2,
+        
+        //Boolean - Whether to fill the dataset with a colour
+        datasetFill : true,
+        
+        //Boolean - Whether to animate the chart
+        animation : true,
+
+        //Number - Number of animation steps
+        animationSteps : 60,
+        
+        //String - Animation easing effect
+        animationEasing : "easeOutQuart",
+
+        //Function - Fires when the animation is complete
+        onAnimationComplete : null
+    }              
+
+    var graph = document.getElementById("historyGraphCanvas").getContext("2d");
+    var myGraph = new Chart(graph).Line(dataGraph, optionsGraph);
+
+    var optionsPie = {
       //Boolean - Whether we should show a stroke on each segment
       segmentShowStroke : true,
       
@@ -209,10 +317,10 @@
         }
     ];
 
-    var myDoughnut1 = new Chart(document.getElementById("canvas1").getContext("2d")).Doughnut(doughnutDataLove, options);
-    var myDoughnut2 = new Chart(document.getElementById("canvas2").getContext("2d")).Doughnut(doughnutDataHealth, options);
-    var myDoughnut3 = new Chart(document.getElementById("canvas3").getContext("2d")).Doughnut(doughnutDataAssets, options);
-    var myDoughnut4 = new Chart(document.getElementById("canvas4").getContext("2d")).Doughnut(doughnutDataMood, options);
+    var myDoughnut1 = new Chart(document.getElementById("canvas1").getContext("2d")).Doughnut(doughnutDataLove, optionsPie);
+    var myDoughnut2 = new Chart(document.getElementById("canvas2").getContext("2d")).Doughnut(doughnutDataHealth, optionsPie);
+    var myDoughnut3 = new Chart(document.getElementById("canvas3").getContext("2d")).Doughnut(doughnutDataAssets, optionsPie);
+    var myDoughnut4 = new Chart(document.getElementById("canvas4").getContext("2d")).Doughnut(doughnutDataMood, optionsPie);
 
   </script>
 @stop
